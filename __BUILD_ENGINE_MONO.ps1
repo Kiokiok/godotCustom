a3ee8c84954f8029db3ba753721693732b11ce68
Gonzalez Damien <gonzalez.damien.kiok@gmail.com>
2023-06-26 14:19:39 +0200

    add stuff for build

--- __BUILD_ENGINE_MONO.ps1
+++ __BUILD_ENGINE_MONO.ps1
@@ -1,5 +1,7 @@
 scons p=windows module_mono_enabled=true
 
+scons platform=windows target=template_debug arch=x86_64 module_mono_enabled=true
+
 .\bin\godot.windows.editor.x86_64.mono.console.exe --headless --generate-mono-glue ./modules/mono/glue
 
-./modules/mono/build_scripts/build_assemblies.py --godot-output-dir ./bin
+py ./modules/mono/build_scripts/build_assemblies.py --godot-output-dir ./bin --push-nupkgs-local bin/NuGet/MyLocalNugetSource
